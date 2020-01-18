df70f61932c56b357ff2545a61dec39a435eac49
nighttardis <henely@gmail.com>
2020-01-18 09:40:49 -0600

    update to worker to control memory and CPU

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-/spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+if [[ -z $SPARK_WORK_CPU_COUNT ]] && [[ -z $SPARK_MEM ]]; then
+  /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+    --webui-port $SPARK_WORKER_WEBUI_PORT \
+    -c $SPARK_WORK_CPU_COUNT \
+    -m $SPARK_MEM \
+    $SPARK_MASTER
+elif [[ -z $SPARK_WORK_CPU_COUNT ]]; then
+  /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+    --webui-port $SPARK_WORKER_WEBUI_PORT \
+    -c $SPARK_WORK_CPU_COUNT \
+    $SPARK_MASTER
+elif [[ -z $SPARK_MEM ]]; then
+  /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+    --webui-port $SPARK_WORKER_WEBUI_PORT \
+    -m $SPARK_MEM \
+    $SPARK_MASTER
+else:
+  /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
     --webui-port $SPARK_WORKER_WEBUI_PORT \
     $SPARK_MASTER
+fi
